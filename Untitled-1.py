fb5b83f969e75a734ad5b3e304514da5693f6d4a
Nidhoog <1214939285@qq.com>
2021-05-07 10:54:44 +0800

    5/7 AM

--- Untitled-1.py
+++ Untitled-1.py
@@ -19,7 +19,56 @@
 # print(len(d.items()))
 # set_name = set([1, 4, 3, 2, 5, 4, 2, 3, 1])
 # print(set_name)
-def jttl(t:int,j:int):
-     message ='鸡的数量:'+str(round((2*t-j/2),0)) +'兔的数量:'+str(round((j/2-t),0))
-     return message
-print(jttl(35,96))
+# def jttl(t:int,j:int): #鸡兔同笼求和函数
+#      message ='鸡的数量:{}'.format(2*t-j/2)+'兔的数量:{}'.format(j/2-t)
+#      return message
+# print(jttl(35,96))
+
+# def sum(num): #求和函数
+#      i = 0
+#      j = 0
+#      if not isinstance(num,int):
+#           message = "输入的值不正确，请输入数值类型"
+#           return message
+#      while i <= num:
+#           j = j + i
+#           i += 1 
+#      return j
+# print(sum(100))
+
+
+# def dg(num):
+#      if num == 1:
+#           return 1
+#      else:
+#           return num +dg(num-1) 
+# print(dg(100))
+
+# def print_test(str1,str2="world"):
+#      if not isinstance(str1,str):
+#           return '输入的值不正确。请输入字符'
+#      else:
+#           if not str1:
+#                return 'hello,{}'.format(str2)
+#           else:
+#                return 'hello,{}'.format(str1)
+# print(print_test('NidhoogJX'))
+
+# def average(*args): 
+#      sum = 0 
+#      for item in args: 
+#           sum += item 
+#           avg = sum / len(args) 
+#           return avg
+# print(average(1,2,3,4,5))
+# def info(**kwargs): 
+#      names = kwargs['names'] 
+#      gender_list = kwargs['gender'] 
+#      age_list = kwargs['age'] 
+#      index = 0 
+#      for name in names: 
+#           gender = gender_list[index] 
+#           age = age_list[index] 
+#           print('name: {}, gender: {}, age: {}'.format(name, gender, age)) 
+#           index += 1 
+# info(names = ['Alice', 'Bob', 'Candy'], gender = ['girl', 'boy', 'girl'], age = [16, 17, 15])
