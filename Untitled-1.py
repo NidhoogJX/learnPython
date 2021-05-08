e82d20af0920580b8d76b4ebd5b8fc7cca6e4ef7
Nidhoog <1214939285@qq.com>
2021-05-08 09:04:57 +0800

    4/7

--- Untitled-1.py
+++ Untitled-1.py
@@ -35,8 +35,6 @@
 #           i += 1 
 #      return j
 # print(sum(100))
-
-
 # def dg(num):
 #      if num == 1:
 #           return 1
@@ -72,6 +70,98 @@
 #           print('name: {}, gender: {}, age: {}'.format(name, gender, age)) 
 #           index += 1 
 # info(names = ['Alice', 'Bob', 'Candy'], gender = ['girl', 'boy', 'girl'], age = [16, 17, 15])
-55
-546
+# class Animal(object): 
+#     __count = 0 
+#     def __init__(self): 
+#         Animal.__count += 1
+#         print(Animal.__count)
+# dog = Animal()
+# cat = Animal()
+# class Anmail(object): pass 
+# dog = Anmail() 
+# cat = Anmail()
+# print(dog)
+# print(cat)
+# dog.name = 'dog'
+# dog.age = 5
+# cat.name = 'dog'
+# cat.age = 5
+# class Animal(object): 
+#     def __init__(self, name, age): 
+#         self.name = name 
+#         self.__age = age 
+# cat = Animal('Kitty', '3') 
+# print(cat.name) 
+# print(cat.__age)
+# class Animal(object): 
+#     __shape = "big"
+#     def __init__(self, name="NifhoogJX", age=10, localtion="china"): 
+#         self.__name = name 
+#         self.__age = age 
+#         self.__localtion = localtion 
+#     def set_shape(self, shape): 
+#         Animal.__shape = shape
+#     def update_info(self, name, age, localtion): 
+#         self.__name = name 
+#         self.__age = age 
+#         self.__localtion = localtion
+#     def get_info(self): 
+#         return 'name = {}, age = {}, localtion = {}'.format(self.__name, self.__age, self.__localtion) 
+#     def get_shape(self):
+#         return Animal.__shape
+# JX = Animal()
+# print(JX.get_info())
+# JX.update_info("jx",22,"suzhou")
+# print(JX.get_info())
+# JX.set_shape("small")
+# print(JX.get_shape())
+# dog = Animal('wangwang', 1, 'GuangDong')
+# class Person(object):
+#     def __init__(self, name, gender):
+#         self.name = name
+#         self.gender = gender
+# class Teacher(Person):
+#         def __init__(self, name, gender, course):
+#             super(Teacher,self).__init__(name,gender)
+#             self.course = course
+# teacher = Teacher('Alice', 'Female', 'English')
+# print(teacher.name)
+# print(teacher.gender)
+# print(isinstance(teacher,object))
+# print(dir(teacher))
+# print(dict(teacher))
+
+
+class Person(object): 
+    pass 
+class Student(Person): 
+    pass 
+class Teacher(Person): 
+    pass 
+class SkillMixin(object): 
+    pass 
+class BasketballMixin(SkillMixin): 
+    def skill(self): 
+        return 'basketball' 
+class FootballMixin(SkillMixin): 
+    def skill(self): 
+        return 'football' 
+class BStudent(BasketballMixin, Student): 
+    pass 
+class FTeacher(FootballMixin, Teacher): 
+    pass 
+s = BStudent() 
+print(s.skill()) 
+t = FTeacher() 
+print(t.skill())
+class Person(object): 
+    def __init__(self, name, gender, **kw): 
+        self.name = name 
+        self.gender = gender 
+        for k, v in kw.items(): 
+            setattr(self, k, v) 
+p = Person('Bob', 'Male', age=18, course='Python') 
+print(p.age) 
+print(p.course)
+
 
